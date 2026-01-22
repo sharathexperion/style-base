6b33d48229b8d804229538919530b5b689fe7ab8
Sharath Daniel <sharath.daniel@experionglobal.com>
2026-01-22 16:09:15 +0530

    updated from routermodule to routerlink

--- src/app/layout/main-header/main-header.ts
+++ src/app/layout/main-header/main-header.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-header',
-  imports: [RouterModule],
+  imports: [RouterLink],
   templateUrl: './main-header.html',
   styleUrl: './main-header.scss',
 })
