a763b610a3511d81085e08d90c7c20861c517265
Sharath <5154064+sharathdaniel@users.noreply.github.com>
2026-02-11 06:39:37 +0530

    icon component input updated to angular 17+ syntax

--- src/app/icons/icon.component.ts
+++ src/app/icons/icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-icon',
@@ -7,21 +7,21 @@ import { Component, Input } from '@angular/core';
     <svg
       class="svg-icon"
       focusable="false"
-      [attr.role]="ariaLabel ? 'img' : null"
-      [attr.aria-hidden]="ariaLabel ? null : 'true'"
-      [attr.aria-label]="ariaLabel"
+      [attr.role]="ariaLabel() ? 'img' : null"
+      [attr.aria-hidden]="ariaLabel() ? null : 'true'"
+      [attr.aria-label]="ariaLabel()"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <use [attr.href]="'#' + name"></use>
+      <use [attr.href]="'#' + name()"></use>
     </svg>
   `,
 })
 export class IconComponent {
-  @Input() name!: string;
+  readonly name = input.required<string>();
   /**
    * Accessibility:
    * - Use `ariaLabel` ONLY for standalone / status icons
    * - NEVER use `ariaLabel` when icon is inside a button or link
    */
-  @Input() ariaLabel?: string;
+  readonly ariaLabel = input<string>();
 }
